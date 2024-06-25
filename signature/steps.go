388a0c05f64e9f151b10a8ab1c84993941bbcf28
Jordan Carter <jordan@buildkite.com>
2024-06-25 14:48:29 +1200

    Options dont need to be initialized in SignSteps, only passed to Sign

--- signature/steps.go
+++ signature/steps.go
@@ -13,11 +13,6 @@ var errSigningRefusedUnknownStepType = errors.New("refusing to sign pipeline con
 // SignSteps adds signatures to each command step (and recursively to any command steps that are within group steps).
 // The steps are mutated directly, so an error part-way through may leave some steps un-signed.
 func SignSteps(s pipeline.Steps, key jwk.Key, repoURL string, opts ...Option) error {
-	options := options{env: make(map[string]string)}
-	for _, o := range opts {
-		o.apply(&options)
-	}
-
 	for _, step := range s {
 		switch step := step.(type) {
 		case *pipeline.CommandStep:
